f4c4bd731efe584440ecf8b40d0d05e1274727eb
Coccisco <Coccisco@67a87a27-6c54-81a0-7c06-4f89fb1c09f5>
2011-05-05 19:47:38 +0000

    Rajout de la fonction qui calcule les stats sur les boules et les étoiles pour le jeu Euromillions

--- Fortuna-ESIEA/src/fr/fortuna/Tirage/Euromillions.java
+++ Fortuna-ESIEA/src/fr/fortuna/Tirage/Euromillions.java
@@ -1,6 +1,10 @@
 package fr.fortuna.Tirage;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 
 public class Euromillions/* implements Jeu*/ {
 	private List<TirageEuromillions> tirages;
@@ -21,6 +25,56 @@ public class Euromillions/* implements Jeu*/ {
 		// TODO Auto-generated method stub
 		
 	}*/
+	
+	/*
+	 * Calcule les statistiques sur les boules et étoiles d'euromillions
+	 * 
+	 * @return liste avec les statistiques 
+	 */
+	public ArrayList<HashMap<Integer, Double>> calculStatEuromillions() {
+		ArrayList<HashMap<Integer, Double>> resultat = new ArrayList<HashMap<Integer, Double>>();	//le retour
+		HashMap<Integer, Double> mapBoule = new HashMap<Integer, Double>(50);	//Map des boules, qui sera rajouté dans l'ArrayList
+		HashMap<Integer, Double> mapEtoile = new HashMap<Integer, Double>(9);	//Pareil pour les étoiles
+		int[] nbBouleTiree = new int[51];
+		int[] nbEtoileTiree = new int[10];
+		int nbTirage = 0;
+		int boule = 0;
+		int etoile = 0;
+		double stat;	//Statistique calculée
+		int i, j;	//Parcours de boucle
+		TirageEuromillions tirageEnCours; //Sauvegarde du tirage dans la boucle
+		
+		Iterator it = tirages.iterator();
+		//On parcourt l'ensemble des tirages
+		while (it.hasNext()) {
+			tirageEnCours = (TirageEuromillions) it.next();
+			//Cas des boules
+			for (i = 0; i < 5; i++) {
+				boule = tirageEnCours.getBoules()[i];
+				nbBouleTiree[boule]++; 
+			}
+			//Cas des étoiles
+			for ( j = 0; j < 2; j++) {
+				etoile = tirageEnCours.getEtoiles()[j];
+				nbEtoileTiree[etoile]++;
+			}
+			nbTirage++;
+		}
+		//Calcul les stats pour les boules
+		for (i = 1; i < nbBouleTiree.length; i++) {
+			stat = nbBouleTiree[i] / nbTirage;
+			mapBoule.put(i, stat);
+		}
+		
+		//Calcul les stats pour les étoiles
+		for (j = 1; j < nbEtoileTiree.length; j++) {
+			stat = nbEtoileTiree[j] / nbTirage;
+			mapEtoile.put(j, stat);
+		}
+		resultat.add(mapBoule);
+		resultat.add(mapEtoile);
+		return resultat;
+	}
 }
 
 
