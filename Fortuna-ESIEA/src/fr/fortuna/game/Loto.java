b864551a25b6e7ccfe45c93750f18d093f7bbc24
Tenkin1 <Tenkin1@67a87a27-6c54-81a0-7c06-4f89fb1c09f5>
2011-05-09 21:24:49 +0000

    Ajout de bouton à la main frame Modification du listener ( rester a ajouter le listener aux bouton, et à le compléter

--- Fortuna-ESIEA/src/fr/fortuna/game/Loto.java
+++ Fortuna-ESIEA/src/fr/fortuna/game/Loto.java
@@ -1,9 +1,12 @@
 package fr.fortuna.game;
 
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.print.attribute.standard.Media;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -16,9 +19,11 @@ public class Loto extends JPanel implements ActionListener{
 	JFrame frame;
 
 	public Loto(JFrame f){
-		super();
+		super(new GridLayout(4, 1, 0, 3));
+
 		this.frame=f;
 		createMenu();
+		addButtons();
 
 	}
 
@@ -33,6 +38,18 @@ public class Loto extends JPanel implements ActionListener{
 		frame.add(new Loto(frame));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
+
+	}
+
+	public void addButtons(){
+
+		JButton b=new JButton("Jouer à Euromillions");
+		this.add(b);
+		b.addActionListener(this);
+
+		b=new JButton("Jouer au Nouveau Loto");
+		this.add(b);
+		b.addActionListener(this);
 
 	}
 
@@ -56,45 +73,33 @@ public class Loto extends JPanel implements ActionListener{
 
 	}
 
-	public void createNewFrameGame(){
-		JFrame newFrame=new JFrame();
-		JDialog dial=new JDialog();
-
-	}
-
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 
-		if ( ((JMenuItem)(e.getSource()) ).getText().equals("Euromillions")){
-			JDialog dial=new JDialog(frame, "Euromillions");
-			dial.setModal(false);
-			dial.setVisible(true);
-			dial.add(new JGrilleEuroMillions(2));
-			dial.pack();
+		if(e.getSource() instanceof JMenuItem) {
+			JMenuItem button=(JMenuItem)(e.getSource());
+			if ( button.getText().equals("Euromillions")){
+				JDialog dial=new JDialog(frame, "Euromillions");
+				dial.setModal(false);
+				dial.setVisible(true);
+				dial.add(new JGrilleEuroMillions(2));
+				dial.pack();
 
-			/*		this.add(new JGrilleEuroMillions(1));
-			this.add(new JGrilleEuroMillions(2));
-			this.add(new JGrilleEuroMillions(3));
-			this.add(new JGrilleEuroMillions(4));
-			 */
 
-			this.validate();frame.pack();
-		}
-		if ( ((JMenuItem)(e.getSource()) ).getText().equals("Nouveau Loto")){
-			JDialog dial=new JDialog(frame, "Nouveau Loto");
-			dial.setModal(false);
-			dial.setVisible(true);
-			dial.add(new JGrilleNouveauLoto());
-			dial.pack();
 
-			/*
-			this.add(new JGrilleNouveauLoto());
+				this.validate();frame.pack();
+			}
+			if ( button.getText().equals("Nouveau Loto")){
+				JDialog dial=new JDialog(frame, "Nouveau Loto");
+				dial.setModal(false);
+				dial.setVisible(true);
+				dial.add(new JGrilleNouveauLoto());
+				dial.pack();
 
-			this.validate();frame.pack();
-			 */
+			}
+
 		}
 
 
