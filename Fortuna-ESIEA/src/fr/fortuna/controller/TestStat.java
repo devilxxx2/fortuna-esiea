d60859903a668cabc3ff19d6d98c8d4b25e6b59c
Coccisco <Coccisco@67a87a27-6c54-81a0-7c06-4f89fb1c09f5>
2011-05-12 00:01:05 +0000

    Test de Calcul de Stat

---
+++ Fortuna-ESIEA/src/fr/fortuna/controller/TestStat.java
@@ -0,0 +1,38 @@
+package fr.fortuna.controller;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import fr.fortuna.dao.NouveauSuperLotoCsvTirageDAO;
+
+public class TestStat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File csv=new File("CSV Files/nouveau_superloto.csv");
+		NouveauSuperLotoCsvTirageDAO t= new NouveauSuperLotoCsvTirageDAO(csv);
+		NouveauSuperLoto eur= new NouveauSuperLoto(t.loadAllTirages());
+		ArrayList<HashMap<Integer, Double>> tableauStat = eur.calculStatJeu();
+		HashMap<Integer, Double> tableauBoule;
+		int i, j;
+		for (i = 0; i < tableauStat.size(); i++) {
+			tableauBoule = tableauStat.get(i);
+			if (i == 0) {		
+				for (j = 1; j <= tableauBoule.size(); j++) {
+					System.out.println("Boule Numéro " + (j) + " : " + tableauBoule.get(j));
+				}
+			}
+			else if (i == 1) {
+				for (j = 1; j <= tableauBoule.size(); j++) {
+					System.out.println("Numero Chance Numéro " + (j) + " : " + tableauBoule.get(j));
+				}
+			}
+		}
+	}
+
+}
