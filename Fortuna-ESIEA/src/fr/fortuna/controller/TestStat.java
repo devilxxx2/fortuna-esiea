19c02e160718ab9e8fa1a16bfa66f021bec10072
Coccisco <Coccisco@67a87a27-6c54-81a0-7c06-4f89fb1c09f5>
2011-05-12 19:28:28 +0000

    Encodage UTF8

--- Fortuna-ESIEA/src/fr/fortuna/controller/TestStat.java
+++ Fortuna-ESIEA/src/fr/fortuna/controller/TestStat.java
@@ -14,25 +14,34 @@ public class TestStat {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
 		File csv=new File("CSV Files/nouveau_superloto.csv");
 		NouveauSuperLotoCsvTirageDAO t= new NouveauSuperLotoCsvTirageDAO(csv);
 		NouveauSuperLoto eur= new NouveauSuperLoto(t.loadAllTirages());
 		ArrayList<HashMap<Integer, Double>> tableauStat = eur.calculStatJeu();
 		HashMap<Integer, Double> tableauBoule;
 		int i, j;
+		/*
 		for (i = 0; i < tableauStat.size(); i++) {
 			tableauBoule = tableauStat.get(i);
 			if (i == 0) {		
 				for (j = 1; j <= tableauBoule.size(); j++) {
-					System.out.println("Boule Numéro " + (j) + " : " + tableauBoule.get(j));
+					System.out.println("Boule NumÃ©ro " + (j) + " : " + tableauBoule.get(j));
 				}
 			}
 			else if (i == 1) {
 				for (j = 1; j <= tableauBoule.size(); j++) {
-					System.out.println("Numero Chance Numéro " + (j) + " : " + tableauBoule.get(j));
+					System.out.println("Numero Chance NumÃ©ro " + (j) + " : " + tableauBoule.get(j));
 				}
 			}
 		}
+		*/
+		TirageEuromillions tirage=new TirageEuromillions();
+		tirage.generate();
+		int[] randomBoules=tirage.getBoules();
+		int[] randomStars=tirage.getEtoiles();
+		System.out.println("ETOILE 1 : " + randomStars[0]);
+		System.out.println("ETOILE 2 : " + randomStars[1]);
 	}
 
 }
