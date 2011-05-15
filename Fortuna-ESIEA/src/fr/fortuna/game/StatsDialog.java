ed46db9407eb82f67ae44c581f85b28bd221af8b
Tenkin1 <Tenkin1@67a87a27-6c54-81a0-7c06-4f89fb1c09f5>
2011-05-15 21:05:26 +0000

    Fenètre faite

---
+++ Fortuna-ESIEA/src/fr/fortuna/game/StatsDialog.java
@@ -0,0 +1,57 @@
+package fr.fortuna.game;
+
+import java.awt.Window;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import fr.fortuna.game.charts.StatChartGagnants;
+import fr.fortuna.game.charts.StatChartGains;
+
+public class StatsDialog extends JDialog {
+
+	JPanel mainPanel, panel;
+
+	public StatsDialog(Window owner, String title, StatChartGagnants chartGagants, StatChartGains chartNums) {
+		super(owner, title);
+
+		JTabbedPane tab=new JTabbedPane(JTabbedPane.BOTTOM);
+		mainPanel=new JPanel();
+
+		tab.addTab("Statistiques gagants",chartGagants);
+		tab.addTab("Statistiques gain",chartNums);
+
+		add(tab);
+
+		setVisible(true);
+		setResizable(false);
+		pack();
+		
+		setModalityType(ModalityType.APPLICATION_MODAL);
+
+	}
+
+/*
+	public static void main(String args[]) {
+		
+		TirageEuromillions tt=new TirageEuromillions();
+		tt.generate();
+		
+		StatChartsGagnantEuromillion ss=new StatChartsGagnantEuromillion("blou", tt);
+		
+		StatChartNumeros chart = new StatChartNumeros("Statistiques",true);
+		chart.pack();
+		RefineryUtilities.centerFrameOnScreen(chart);
+		chart.setVisible(true);
+		StatChartNumeros chart1 = new StatChartNumeros("Statistiques",false);
+		chart1.pack();
+		RefineryUtilities.centerFrameOnScreen(chart1);
+		chart1.setVisible(true);
+		new StatsDialog(new JFrame(), "Bloup", ss, chart);
+
+
+	}
+*/
+
+}
