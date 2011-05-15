c893de36925ef7d7f186c76f6d8a0a7ebddd63d9
oriane.dorey@gmail.com <oriane.dorey@gmail.com@67a87a27-6c54-81a0-7c06-4f89fb1c09f5>
2011-05-15 20:48:33 +0000

    update

--- Fortuna-ESIEA/src/fr/fortuna/game/charts/StatChartGainsLoto.java
+++ Fortuna-ESIEA/src/fr/fortuna/game/charts/StatChartGainsLoto.java
@@ -1,5 +1,37 @@
 package fr.fortuna.game.charts;
 
-public class StatChartGainsLoto {
+import java.util.HashMap;
+
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import fr.fortuna.controller.Tirage;
+import fr.fortuna.controller.TirageLoto;
+
+public class StatChartGainsLoto extends StatChartGains{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private HashMap<Integer, Double> result;
+
+	public StatChartGainsLoto(String title, TirageLoto tirage) {
+		super(title, tirage);
+		result=tirage.calculStatGain();
+	}
+
+	@Override
+	protected CategoryDataset createDataset() {
+		Double value = 0.0;// La statistique à afficher
+		DefaultCategoryDataset defaultcategorydataset = new DefaultCategoryDataset();
+		// récupération de la stat par numéro/étoile
+		for (int i = 0; i < result.size(); ++i) {
+			value = result.get(i);
+			System.out.println(value);
+			defaultcategorydataset.addValue(value, "Rang",String.valueOf(i));
+		}
+		return defaultcategorydataset;
+	}
 
 }
